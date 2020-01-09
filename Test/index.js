293060a1b3e35b28e073667a23766c5ab2caf59f
Sean Hewitson <sean.hewitson@therange.co.uk>
2020-01-09 20:44:52 +0000

    UPDATE - Added design changes to test page

--- Test/index.js
+++ Test/index.js
@@ -8,6 +8,7 @@ module.exports = function(){
     var priorities = ["Low", "Medium", "High"];
     var statuses = ["Raised", "In Progress", "Testing", "Completed"];
     for(var i = 1; i <= 10; i++){
+        var status = statuses[Math.floor(Math.random()*statuses.length)];
         bugs.push({
             ID: i,
             description: `Bug ${i} Description`,
@@ -15,7 +16,8 @@ module.exports = function(){
             component: `Bug ${i} Component`,
             type: types[Math.floor(Math.random()*types.length)],
             priority: priorities[Math.floor(Math.random()*priorities.length)],
-            status: statuses[Math.floor(Math.random()*statuses.length)],
+            status: status,
+            colour: status == "Completed" ? "table-success" : status == "Testing" ? "table-warning" : status == "In Progress" ? "table-primary" : "table-danger"
         });
     }
     router.get('/', function(req, res, next){
